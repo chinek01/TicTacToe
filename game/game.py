6b4b4af5fb38037dca033d6279f387e4713db748
MarcinC <chinek01@gmail.com>
2023-06-28 18:31:39 +0200

    init game class functions: - set_symbol_on_field - check_field

--- game/game.py
+++ game/game.py
@@ -10,4 +10,52 @@ Author: MC
 class Game:
 
     def __init__(self):
-        pass
+        self.area = ['-', '-', '-',
+                     '-', '-', '-',
+                     '-', '-', '-']
+
+    def set_symbol_on_field(self,
+                            symbol,
+                            field):
+        """
+        Set symbol X or O on area field
+        :param symbol: X or O
+        :param field: field number from 0 to 8
+        :return: True - , False -
+        """
+
+        if symbol is None:
+            raise ValueError("Symbol must be set!")
+
+        if field is None:
+            raise ValueError("Field must be set!")
+
+    def check_field(self,
+                    field):
+        """
+        Check the field is empty
+        :param field: field number from 0 to 8
+        :return: True - field is empty, False - field is'n empty
+        """
+
+        if field is None:
+            raise ValueError('Field must be set!')
+
+        if not (0 <= field <= 8):
+            raise AttributeError('Field number must by from 0 to 8')
+
+        for f in self.area:
+            # print(f)
+            if f != '-':
+                return False
+
+        return True
+
+
+# some class test
+if __name__ == '__main__':
+    game = Game()
+    print(f"7: {game.check_field(7)}")
+    # print(f"19: {game.check_field(19)}")
+    # print(f"-9: {game.check_field(-9)}")
+    # print(game.check_field())
