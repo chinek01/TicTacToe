a8a73719979b152ace13cf6cfaf554596bd7d313
Marcin Chruszcz <chinek01@o2.pl>
2023-06-28 10:18:19 +0200

    Init project

---
+++ art/art.py
@@ -0,0 +1,9 @@
+"""
+
+Some art for project
+Start date: 2023-06-28
+Author: MC
+
+"""
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+
+Główny plik projektu TicTacToe -> Kółko i krzyżyk
+#100DaysOfCode with Python
+Day: 83
+Start date: 2023-06-28
+Author: MC
+
+"""
