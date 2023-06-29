50dd20b2dbc842f8491db79126e813521662479b
MarcinC <chinek01@gmail.com>
2023-06-29 13:27:31 +0200

    merge game and gamer brach into main branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 
-Główny plik projektu TicTacToe -> Kółko i krzyżyk
+Główny plik projektu Tic Tac Toe -> Kółko i krzyżyk
 #100DaysOfCode with Python
 Day: 83
 Start date: 2023-06-28
@@ -9,5 +9,7 @@ Author: MC
 """
 
 import art.art as art
+from game.game import Game
+from gamer.gamer import Gamer
 
 print(art.logo)
