5c4be96c2e779d8a47ebb3fc41b6e3f8462726b6
MarcinC <chinek01@gmail.com>
2023-06-30 13:28:43 +0200

    define clear scr function

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ Author: MC
 
 """
 
+from time import sleep
+import os
+
 import art.art as art
 from game.game import Game
 from gamer.gamer import Gamer
 
+
+def clear():
+
+    # for windows
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
 print(art.logo)
+sleep(5)
+clear()
