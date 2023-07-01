6d4ecde582b13b28922ce8be703107ecbccce2db
MarcinC <chinek01@gmail.com>
2023-07-01 09:52:46 +0200

    main loop for the game ane question about continue

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def clear():
 p1_symbol = 'X'
 p2_symbol = 'O'
 
+flag_new_game = True
+
 # print logo
 print(art.logo)
 sleep(3)
@@ -43,47 +45,54 @@ print(art.set_player_name(2))
 player_2 = Gamer(input("Name: "))
 clear()
 
-# choice who player will be first
-first_move = randint(1, 2)
+# main app loop
+while flag_new_game:
 
-# set symbols for players
-if first_move == 1:
-    player_1.set_symbol(p1_symbol)
-    player_2.set_symbol(p2_symbol)
-else:
-    player_1.set_symbol(p2_symbol)
-    player_2.set_symbol(p1_symbol)
+    # choice who player will be first
+    first_move = randint(1, 2)
 
-# init game class - if new game init new object :)
-my_game = Game()
-
-while not my_game.end_game_flag and not my_game.dead_heat_flag:
-
-    print()
-    print(f"""
-end_game flag: {my_game.end_game_flag}
-dead-heat flag: {my_game.dead_heat_flag}
-""")
-    my_game.print_area_gui()
-
+    # set symbols for players
     if first_move == 1:
-        # wait for player_1 move
-        print(art.set_player_move(player_1.name, player_1.symbol))
-        my_game.set_symbol_on_field(player_1.get_symbol(),
-                                    int(input("Field: ")))
-
-        if my_game.end_game_flag is True:
-            player_1.increase_score()
-
-        first_move = 2
+        player_1.set_symbol(p1_symbol)
+        player_2.set_symbol(p2_symbol)
     else:
-        # wait for player_2 move
-        print(art.set_player_move(player_2.name, player_2.symbol))
-        my_game.set_symbol_on_field(player_2.get_symbol(),
-                                    int(input("Field: ")))
+        player_1.set_symbol(p2_symbol)
+        player_2.set_symbol(p1_symbol)
 
-        if my_game.end_game_flag is True:
-            player_2.increase_score()
-        first_move = 1
+    # init game class - if new game init new object :)
+    my_game = Game()
 
-    clear()
+    while not my_game.end_game_flag and not my_game.dead_heat_flag:
+
+        print()
+        print(f"""
+    end_game flag: {my_game.end_game_flag}
+    dead-heat flag: {my_game.dead_heat_flag}
+    """)
+        my_game.print_area_gui()
+
+        if first_move == 1:
+            # wait for player_1 move
+            print(art.set_player_move(player_1.name, player_1.symbol))
+            my_game.set_symbol_on_field(player_1.get_symbol(),
+                                        int(input("Field: ")))
+
+            if my_game.end_game_flag is True:
+                player_1.increase_score()
+
+            first_move = 2
+        else:
+            # wait for player_2 move
+            print(art.set_player_move(player_2.name, player_2.symbol))
+            my_game.set_symbol_on_field(player_2.get_symbol(),
+                                        int(input("Field: ")))
+
+            if my_game.end_game_flag is True:
+                player_2.increase_score()
+            first_move = 1
+
+        clear()
+
+    # new game question
+    if input("Another game [Y/N]: ").upper() != 'Y':
+        flag_new_game = False
